99b67113ebbf581626f5f1680d1bce11753716a7
Rubai Rahman <rubairahman1@gmail.com>
2026-09-09 13:38:50 +0600

    account type updated in signupform

--- src/constants/signup.constant.ts
+++ src/constants/signup.constant.ts
@@ -1,5 +1,10 @@
 export const accountType = [
-  { value: 1, label: 'Campus Ambassador' },
+  { value: 1, label: 'Content Creator' },
   { value: 2, label: 'Influencer' },
-  { value: 3, label: 'Affiliate Marketer' },
+  { value: 3, label: 'Campus Ambassador' },
+  { value: 4, label: 'Website Publisher' },
+  { value: 5, label: 'Tech Reviewer' },
+  { value: 6, label: 'Community Admin' },
+  { value: 7, label: 'Affiliate Marketer' },
+  { value: 8, label: 'Others' },
 ];
